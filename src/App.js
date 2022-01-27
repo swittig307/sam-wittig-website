b2a9cd53ab23076d56b7f447e9edbe7579cc6e41
Samantha Wittig <swittig307@gmail.com>
2022-01-26 20:32:57 -0500

    Change Sam to Samantha

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Sam Wittig</h3>
+        <h3>Samantha Wittig</h3>
       </header>
       <Carousel className="Carousel">
         {headshots.map((image) => (
